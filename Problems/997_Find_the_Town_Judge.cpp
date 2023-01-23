d4a6779515c89e5a61740228217ae68c3f352110
ibeastking <omajitpatil@gmail.com>
2023-01-23 23:15:32 +0530

    modification

--- Problems/997_Find_the_Town_Judge.cpp
+++ Problems/997_Find_the_Town_Judge.cpp
@@ -23,9 +23,11 @@ public:
         vector<int> count(n + 1);
         for (auto person : trust)
         {
-            //* count[person[0]]--; decrements the trust count of the first person in the pair. This is done because if the first person trusts the second person, it means that the first person does not trust themselves.
+            //* count[person[0]]--; decrements the trust count of the first person in the pair. This is done because if the first person trusts the second person,
+            //* it means that the first person does not trust themselves.
             count[person[0]]--;
-            //* count[person[1]]++; increments the trust count of the second person in the pair. This is done because if the first person trusts the second person, it means that the second person is trusted by the first person.
+            //* count[person[1]]++; increments the trust count of the second person in the pair. This is done because if the first person trusts the second person,
+            //* it means that the second person is trusted by the first person.
             count[person[1]]++;
         }
 
