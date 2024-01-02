052a9ab320f3dd8e625b32f879503430bce47b0b
ibeastking <surajkansagara@gmail.com>
2024-01-02 08:55:58 +0530

    java

--- Contest/Maximum_Square_Area_by_Removing_Fences_From_a_Field.java
+++ Contest/Maximum_Square_Area_by_Removing_Fences_From_a_Field.java
@@ -5,8 +5,12 @@ import java.util.HashMap;
 
 class Solution {
 	public int maximizeSquareArea(int m, int n, int[] hFences, int[] vFences) {
+
+        //* Create a HashMap to store the horizontal lengths */
 		HashMap<Integer, Integer> map = new HashMap<>(Map.of(m - 1, m - 1));
-        
+
+        //* iterate through all possible lengths which can be 
+        //* obtained by removing fences, remember to store the abs value of length
 		for (int i = 0; i < hFences.length; i++) {
 			map.put(hFences[i] - 1, hFences[i] - 1);
 			map.put(m - hFences[i], m - hFences[i]);
@@ -15,7 +19,11 @@ class Solution {
 			}
 		}
 
+        //* first chech if there exist a length where no vertical fences available
+        //* indirectly that will be the longest length then
 		long max = map.getOrDefault(n - 1, 0);
+
+        //* iterate through all vertical fences and check if that value already exists in map
 		for (int i = 0; i < vFences.length; i++) {
 			max = Math.max(max, Math.max(map.getOrDefault(vFences[i] - 1, 0), map.getOrDefault(n - vFences[i], 0)));
 			for (int j = i + 1; j < vFences.length; j++) {
